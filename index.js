8a04a408e0df1d5339012c8444bf408c7fba727e
pixel_M <71042423+pIXELMDev84@users.noreply.github.com>
2020-10-20 17:32:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client(),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
